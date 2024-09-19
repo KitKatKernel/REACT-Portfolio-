cea2fc084e65d37092488e24e165aa0a1130760b
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-09-18 22:38:56 -0400

    Set up basic header and footer components.

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/KitKatKernel" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/in/james-ryan-iv-493bb0308/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="mailto:Natecrunchyroll@gmail.com">Email</a>
+    </footer>
+  );
+}
+
+export default Footer;
